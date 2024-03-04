faad6538aa78f774b6a63528501f2c2e2f53f62b
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-05 00:30:33 +0300

    finalize the project

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,54 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (true) {
+            String[] rps = {"r", "p", "s"};
+            String computerMove = rps[new Random().nextInt(rps.length)];
+            String playerMove;
+
+            while (true) {
+                System.out.println("Please enter your move (r, p, or s)");
+                playerMove = scanner.nextLine();
+                if (playerMove.equals("r") || playerMove.equals("p") || playerMove.equals("s")) {
+                    break;
+                }
+                System.out.println(playerMove + " is not a valid move.");
+            }
+
+            System.out.println("Computer played: " + computerMove);
+            if (playerMove.equals(computerMove)) {
+                System.out.println("The game was a tie!");
+            } else if (playerMove.equals("r")) {
+                if (computerMove.equals("p")) {
+                    System.out.println("You lose!");
+                } else if (computerMove.equals("s")) {
+                    System.out.println("You win!");
+                }
+            } else if (playerMove.equals("p")) {
+                if (computerMove.equals("r")) {
+                    System.out.println("You win!");
+                } else if (computerMove.equals("s")) {
+                    System.out.println("You lose!");
+                }
+            } else if (playerMove.equals("s")) {
+                if (computerMove.equals("p")) {
+                    System.out.println("You win!");
+                } else if (computerMove.equals("r")) {
+                    System.out.println("You lose!");
+                }
+            }
+
+            System.out.println("Play again? (y/n)");
+            String playAgain = scanner.nextLine();
+
+            if (!playAgain.equals("y")) {
+                break;
+            }
         }
+        scanner.close(); // Closing scanner outside the loop
     }
 }
